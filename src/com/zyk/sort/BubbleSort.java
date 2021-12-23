ca48fdd0532fe2ef9be1f0a07cf39fd01148bd92
BestKang <997796647@qq.com>
2021-12-24 07:39:04 +0800

    提交

---
+++ src/com/zyk/sort/BubbleSort.java
@@ -0,0 +1,36 @@
+package com.zyk.sort;
+
+import java.util.Date;
+
+/**
+ * 冒泡排序
+ */
+public class BubbleSort {
+    public static Integer[] sort(Integer[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if (arr[j]>arr[j+1]){
+                    SortTestHelper.swap(arr,j,j+1);
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int N = 20000;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100);
+        /*for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }*/
+        long startTime = new Date().getTime();
+        BubbleSort.sort(arr);
+        long endTime = new Date().getTime();
+        System.out.println("耗时："+(endTime - startTime));
+        System.out.println(SortTestHelper.checkIsSort(arr));
+        /*for( int i = 0 ; i < arr.length ; i ++ ){
+            System.out.print(arr[i]);
+            System.out.print(' ');
+        }*/
+    }
+}
--- src/com/zyk/sort/ShellSort.java
+++ src/com/zyk/sort/ShellSort.java
@@ -3,7 +3,7 @@ package com.zyk.sort;
 import java.util.Date;
 
 /**
- * 功能描述:
+ * 功能描述:  希尔排序
  *
  * @author zyk
  * @date 2021/12/23 16:09
@@ -13,38 +13,53 @@ public class ShellSort {
     public static Integer[] sort(Integer[] arr){
         int gap = arr.length/2;
         int temp ;
-        for (gap = arr.length/2; gap < arr.length &&gap>0; gap=gap/2) {
+        for (gap = arr.length/2; gap>0; gap=gap/2) {
             //gap =gap/2;
-            for (int i = 0; i < gap; i++) {
-                for (int j = i; j <arr.length-gap ; j=j+gap) {
-                    if (arr[j]>arr[j+gap]){
-                        temp = arr[j];
-                        arr[j] = arr[j+gap];
-                        arr[j+gap]=temp;
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i; j >=gap ; j=j-gap) {
+                    if (arr[j]<arr[j-gap]){
+                        swap(arr,j,j-gap);
                     }else{
                         break;
                     }
                 }
             }
         }
+        /**
+         * 插入排序
+         */
+        /*for (int i = 0; i <arr.length ; i++) {
+            for (int j = i; j >0; j--) {
+                if (arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }else {
+                    break;
+                }
+            }
+        }*/
         return arr;
     }
-
+    //核心代码---结束
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
     public static void main(String[] args) {
 
-        int N = 200;
-        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 1000000);
-        /*for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }*/
+        int N = 20;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"  ");
+        }
         long startTime = new Date().getTime();
         ShellSort.sort(arr);
         long endTime = new Date().getTime();
         System.out.println("耗时："+(endTime - startTime));
         System.out.println(SortTestHelper.checkIsSort(arr));
-        /*for( int i = 0 ; i < arr.length ; i ++ ){
+        for( int i = 0 ; i < arr.length ; i ++ ){
             System.out.print(arr[i]);
             System.out.print(' ');
-        }*/
+        }
     }
 }
